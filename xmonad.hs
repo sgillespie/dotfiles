e68eb6582e96f1f6177e58595738b8aa95619a7e
Sean Gillespie <sean@mistersg.net>
2016-11-04 19:59:13 -0400

    Update xmonad config--correct volume bindings

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Replace
+
 import System.IO
 
 main :: IO ()
@@ -27,14 +28,19 @@ config' = defaultConfig {
 additionalKeys' = flip additionalKeys $
   [((mod4Mask .|. shiftMask, xK_l), spawn "sxlock -f '-*-ubuntu-medium-r-normal-*-30-*-*-*-*-*-iso8859-1'"),
    ((0, xK_Print), spawn "import -window root /home/sgillespie/documents/screenshots/$(date +%Y%m%d%H%M%S).png"),
+   ((0, 0x1008FF12), spawn "amixer sset Master toggle"),
+   ((0, 0x1008FF11), spawn "amixer sset Master 5%-"),
+   ((0, 0x1008FF13), spawn "amixer sset Master 5%+"),
    ((mod4Mask, xK_Print), spawn "import -window 0 /home/sgillespie/documents/screenshots/$(date +%Y%m%d%H%M%S).png"),
-   ((mod4Mask .|. mod1Mask, xK_e), spawn "emacs"),
+   ((mod4Mask .|. mod1Mask, xK_Return), spawn "urxvt -e bash -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -s$USER@$HOSTNAME\""),
    ((mod4Mask .|. mod1Mask, xK_c), spawn "chromium"),
+   ((mod4Mask .|. mod1Mask, xK_e), spawn "emacs"),
    ((mod4Mask .|. mod1Mask, xK_f), spawn "firefox"),
    ((mod4Mask .|. mod1Mask, xK_i), spawn "idea.sh"),
-   ((mod4Mask .|. mod1Mask, xK_Return), spawn "urxvt -e bash -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -s$USER@$HOSTNAME\""),
+   ((mod4Mask .|. mod1Mask, xK_f), spawn "gvim"),
    ((mod4Mask .|. mod1Mask, xK_t), spawn "tor-browser-en"),
    ((mod4Mask .|. mod1Mask, xK_o), restart "/home/sgillespie/.xmonad/obtoxmd.sh" True)]
 
 manageHook' = composeAll
               [className =? "jetbrains-idea-ce" --> doFloat]
+
