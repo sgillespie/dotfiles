e4ce7149ae7dbb033bc3c3512ad22adf01c739f0
Sean D Gillespie <sean@mistersg.net>
2017-03-27 11:41:02 +0000

    Make zshrc great again

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,40 +1,23 @@
 #!/bin/zsh
 
 # Find script location
-SCRIPT_PATH="$HOME/.zshrc"	# Assume ~/.zshrc
-
-while [[ -h "$SCRIPT_PATH" ]]; do
-  SCRIPT_PATH="$(readlink "$SCRIPT_PATH")"
+# From https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh#23259585
+# and http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE=${(%):-%N}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-if [[ ! $SCRIPT_DIR =~ ^/ ]]; then
-    SCRIPT_DIR="$HOME/$SCRIPT_DIR"
-fi
-
-export INIT_SCRIPT_DIR=${SCRIPT_DIR}
+export INIT_SCRIPT_DIR=${DIR}
 
 # Load files in ~/.zsh.d
 if [[ -d ~/.zsh.d ]]; then
     for src in $(find ~/.zsh.d -type f -name \*.zsh); do
 	source "$src"
     done
-fi
-
-# Find platform
-case $(uname) in
-    Linux)
-        source $SCRIPT_DIR/zsh.conf/linux.sh
-        ;;
-    Darwin)
-        source $SCRIPT_DIR/zsh.conf/osx.sh
-        ;;
-esac
-
-# Find accurev
-accurev=$(whence accurev)
-if [[ $? -eq 0 ]]; then
-    source $SCRIPT_DIR/zsh.conf/accurev.zsh
 fi
 
 # zsh completion
@@ -55,14 +38,9 @@ export EDITOR="$EC"
 # make sure color output is set
 export CLICOLOR=
 
-export GRADLE_OPTS="-Xmx1024m -XX:MaxPermSize=128m -Djavax.net.ssl.trustStore=/Users/sgillespie/local/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit"
-
 function ecbuffer {
     TMP="$(mktemp /tmp/emacsstdinXXX)"
     cat > $TMP
     ec --alternate-editor /usr/bin/false --eval "(let ((b (create-file-buffer \"*stdin*\"))) (switch-to-buffer b) (insert-file-contents \"${TMP}\"))"
     rm $TMP
 }
-
-#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
-[[ -s "/Users/sgillespie/.gvm/bin/gvm-init.sh" ]] && source "/Users/sgillespie/.gvm/bin/gvm-init.sh"
