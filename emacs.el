815bce84dd14964f27ef3c4805384a854d1e05b7
Sean Gillespie <sgillespie@fanatics.com>
2018-10-30 15:33:45 -0400

    Rewrite all of emacs config
    Use use-package instead of adhoc package names

--- emacs.el
+++ emacs.el
@@ -1,158 +1,19 @@
 ;;; Start the emacs server
 (add-hook 'after-init-hook 'server-start)
 
-;;; Global Settings
-(require 'compile)
-(require 'tramp)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["black" "red" "green4" "yellow4" "blue3" "magenta4" "cyan4" "white"])
- '(auto-save-default nil)
- '(auto-save-mode nil)
- '(backup-directory-alist (quote (("." . "~/.emacs_backups"))))
- '(compilation-environment (quote ("TERM=xterm-256color")))
- '(compilation-scroll-output 'first-error)
- '(compile-command "./gradle.sh build")
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(fill-column 90)
- '(indent-tabs-mode nil)
- '(js-indent-level 2)
- '(js2-allow-member-expr-as-function-name t)
- '(js2-basic-offset 2)
- '(js2-missing-semi-one-line-override t)
- '(js2-mode-show-parse-errors nil)
- '(js2-mode-show-strict-warnings nil)
- '(js2-strict-cond-assign-warning nil)
- '(js2-strict-inconsistent-return-warning nil)
- '(js2-strict-missing-semi-warning nil)
- '(js2-strict-trailing-comma-warning nil)
- '(js2-strict-var-hides-function-arg-warning nil)
- '(js2-strict-var-redeclaration-warning nil)
- '(make-backup-files nil)
- '(neo-window-width 35)
- '(nxml-child-indent 2)
- '(package-archives
-   (quote
-    (("melpa" . "http://melpa.org/packages/")
-     ("elpa" . "http://elpa.gnu.org/packages/"))))
- '(package-selected-packages
-   (quote
-    (idris-mode intero magit magit-filenotify magit-find-file emacsql emacsql-mysql smex neotree ido-ubiquitous find-file-in-repository exec-path-from-shell nix-mode web-mode json-mode js2-mode flycheck groovy-mode ghc haskell-mode color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme sr-speedbar)))
- '(speedbar-frame-parameters
-   (quote
-    ((width . 50)
-     (minibuffer)
-     (border-width . 0)
-     (menu-bar-lines . 0)
-     (tool-bar-lines . 0)
-     (unsplittable . t)
-     (left-fringe . 0))))
- '(speedbar-show-unknown-files t)
- '(speedbar-update-flag nil)
- '(sr-speedbar-auto-refresh nil)
- '(truncate-lines t)
- '(web-mode-indent-offset 2))
-
-(add-to-list 'load-path "~/.emacs.d/elisp")
-
-;;; Packages
-(require 'package)
-(package-initialize)
-
-(defvar install-packages '(; Themes
-			   color-theme
-                           color-theme-sanityinc-tomorrow
-			   color-theme-sanityinc-solarized
-
-                           ; languages
-                           dockerfile-mode
-                           haskell-mode
-                           idris-mode
-                           intero
-			   ghc
-                           groovy-mode
-                           yaml-mode
-
-                           ; javascript
-                           flycheck
-                           js2-mode
-                           json-mode
-                           web-mode
-
-                           ; nix
-                           nix-mode
-
-                           ; sql
-                           emacsql
-                           emacsql-mysql
-
-                           ; Misc
-                           exec-path-from-shell
-                           find-file-in-repository
-                           ido-completing-read+
-                           magit
-                           neotree
-                           restclient
-                           smex))
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(dolist (p install-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-;;; Mode-specific Settings
-(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rest$" . restclient-mode))
-
-;; Haskell mode
-(intero-global-mode 1)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-(add-hook 'haskell-mode-hook
-          (lambda ()
-            (setq compile-command "stack build --fast --test")))
-(flycheck-add-next-checker
- 'intero
- '(warning . haskell-hlint))
+;;; UI
+(line-number-mode 1)
+(column-number-mode 1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(global-hl-line-mode 1)
 
 ;; Make shell mode nicer
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-;; This stuff is to ansi-colorize the compilation buffer after a rails test so the terminal colors come through.
-(define-derived-mode ansi-compilation-mode compilation-mode "ansi compilation"
-  "Compilation mode that understands ansi colors."
-  (require 'ansi-color)
-  (read-only-mode 0)
-  (ansi-color-apply-on-region (point-min) (point-max)))
-
-(defun colorize-compilation (one two)
-  "Ansi colorize the compilation buffer."
-  (ansi-compilation-mode))
-
-(setq compilation-finish-function
-      'colorize-compilation)
-
-; What happened here?
-; (bury-successful-compilation t)
-
-;; Override disabled keys
-(put 'downcase-region 'disabled nil)
-
-;;; Load environment from shell
-(when window-system
-  (exec-path-from-shell-initialize))
+;; Font
+(add-to-list 'default-frame-alist '(font . "Source Code Pro:pixelsize=16:weight=normal"))
 
 ;;; Key Bindings
 (defvar personal-keys-minor-mode-map
@@ -167,18 +28,9 @@
 
 (personal-keys-minor-mode 1)
 
-; Disable my keys in minibuffer
+;; Disable my keys in minibuffer
 (add-hook 'minibuffer-setup-hook
           (lambda () (personal-keys-minor-mode 0)))
-
-(ido-mode 1)
-(ido-everywhere 1)
-(ido-ubiquitous-mode 1)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 (defun prev-window ()
   (interactive)
@@ -196,80 +48,194 @@
 (global-set-key (kbd "C-c r") 'recompile)
 (global-set-key (kbd "C-c C-l") 'visual-line-mode)
 
-(setq site-el "/usr/share/emacs/site-lisp/")
-(setq home-el "~/.emacs.d/")
+;; Override disabled keys
+(put 'downcase-region 'disabled nil)
 
-;; Mutt support.
-(add-to-list 'auto-mode-alist '("/tmp/mutt.*" . mail-mode))
+;;; Packages
+(require 'package)
+(package-initialize)
 
-;;; Colors
-(require 'color-theme)
-(color-theme-sanityinc-tomorrow-night)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-;;; Flycheck
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
 
-(flycheck-add-mode 'javascript-eslint 'js-mode)
-(flycheck-add-mode 'javascript-eslint 'js2-mode)
-(setq-default flycheck-disabled-checkers
-  (append flycheck-disabled-checkers
-	  '(javascript-jshint)))
+;; Themes
+(use-package color-theme
+  :ensure t)
 
-;;; Compilation
-(add-hook 'compilation-mode-hook
-          (lambda () (font-lock-mode -1)))
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :config (color-theme-sanityinc-tomorrow-night))
 
-;;; Groovy
-(autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
-(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+;; Languages
+(use-package dockerfile-mode
+  :ensure t)
 
-;;; Javascript
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-(add-to-list 'auto-mode-alist '("package\\.json" . json-mode))
-(add-to-list 'auto-mode-alist '("\\.babelrc" . json-mode))
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (local-set-key (kbd "RET") 'newline-and-indent)
-            (set (make-local-variable 'electric-indent-functions)
-                 (list (lambda (arg)
-                         'no-indent)))))
+(use-package emacsql
+  :ensure t)
 
+(use-package emacsql-mysql
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :hook (after-init . global-flycheck-mode)
+  :config (flycheck-add-mode 'javascript-eslint 'js-mode)
+          (flycheck-add-mode 'javascript-eslint 'js2-mode)
+          (setq-default flycheck-disabled-checkers
+            (append flycheck-disabled-checkers '(javascript-jshint))))
+
+(use-package ghc
+  :ensure t)
+
+(use-package groovy-mode
+  :ensure t
+  :mode (("\\.groovy$" . groovy-mode)
+         ("\\.gradle$" . groovy-mode))
+  :interpreter "groovy"
+  :hook (groovy-mode . java-like-mode-hook))
+
+(use-package haskell-mode
+  :ensure t
+  :config (setq compile-command "stack build --fast --test")
+  :hook (haskell-mode . haskell-indentation-mode))
+
+(use-package idris-mode
+  :ensure t)
+
+(use-package intero
+  :ensure t
+  :after (fly-check)
+  :config (intero-global-mode 1)
+          (flycheck-add-next-checker
+           'intero
+           '(warning . haskell-hlint)))
+
+(use-package js2-mode
+  :ensure t
+  :mode "\\.js$"
+  :bind (:map js2-mode-map
+          ("RET" . newline-and-indent))
+  :config (set (make-local-variable 'electric-indent-functions)
+               (list (lambda (one two)
+                       'no-indent))))
+
+(use-package json-mode
+  :ensure t
+  :mode (("package\\.json" . json-mode)
+         ("\\.babelrc$" . json-mode)))
+
+(use-package ruby-mode
+  :mode "capfile")
+
+(use-package nix-mode
+  :ensure t)
+
+(use-package web-mode
+  :ensure t
+  :mode "\\.html$")
+
+(use-package yaml-mode
+  :ensure t)
+
+;; Others
+(use-package ansi-color
+  :ensure t
+  :config
+  (setq compilation-finish-functions
+        'colorize-compilation))
+  
+(use-package exec-path-from-shell
+  :if window-system
+  :ensure t
+  :config (exec-path-from-shell-initialize))
+
+(use-package find-file-in-repository
+  :ensure t)
+
+(use-package ido-completing-read+
+  :ensure t
+  :config (ido-mode 1)
+          (ido-everywhere 1)
+          (ido-ubiquitous-mode 1))
+
+(use-package magit
+  :ensure t
+  :bind ("C-c g" . magit-status))
+
+(use-package neotree
+  :ensure t
+  :bind (("C-c f" . neotree-show)
+         ("C-c C-f" . neotree-toggle)))
+
+(use-package restclient
+  :ensure t
+  :mode "\\.rest$")
+
+(use-package smex
+  :ensure t
+  :bind (("M-x" . smex)
+         ("M-X" . smex-major-mode-commands)
+         ("C-c C-c M-x" . execute-extended-command)))
+
+(use-package use-package
+  :ensure t)
+
+;; Java-esque indenting
 (defun java-like-mode-hook ()
   (setq c-basic-offset 2
         tab-width 2
         indent-tabs-mode nil))
 
 (add-hook 'java-mode-hook 'java-like-mode-hook)
-(add-hook 'groovy-mode-hook 'java-like-mode-hook)
 
-;;; UI
-(line-number-mode 1)
-(column-number-mode 1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(global-hl-line-mode 1)
-; (desktop-save-mode 1)
+;; ansi-colorize after a test so terminal colors come through.
+(define-derived-mode ansi-compilation-mode compilation-mode "ansi compilation"
+  "Compilation mode that understands ansi colors."
+  (read-only-mode 0)
+  (ansi-color-apply-on-region (point-min) (point-max)))
+  
+(defun colorize-compilation ()
+  "Ansi colorize the compilation buffer."
+  (ansi-compilation-mode))
 
-;;; magit
-(define-key personal-keys-minor-mode-map
-  (kbd "C-c g") 'magit-status)
-
-;;; neotree
-(require 'neotree)
-
-(define-key personal-keys-minor-mode-map
-  (kbd "C-c f") 'neotree-show)
-(define-key personal-keys-minor-mode-map
-  (kbd "C-c C-f") 'neotree-toggle)
-
-;;; fonts
-(add-to-list 'default-frame-alist '(font . "Source Code Pro:pixelsize=16:weight=normal"))
-
+;;; Custom variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-mode nil)
+ '(backup-directory-alist (quote (("." . "~/.emacs_backups"))))
+ '(compilation-environment (quote ("TERM=xterm-256color")))
+ '(compilation-scroll-output (quote first-error))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fill-column 90)
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(js2-allow-member-expr-as-function-name t)
+ '(js2-basic-offset 2)
+ '(js2-missing-semi-one-line-override t)
+ '(js2-mode-show-parse-errors nil)
+ '(js2-mode-show-strict-warnings nil)
+ '(js2-strict-cond-assign-warning nil)
+ '(js2-strict-inconsistent-return-warning nil)
+ '(js2-strict-missing-semi-warning nil)
+ '(js2-strict-trailing-comma-warning nil)
+ '(js2-strict-var-hides-function-arg-warning nil)
+ '(js2-strict-var-redeclaration-warning nil)
+ '(make-backup-files nil)
+ '(neo-window-width 35)
+ '(nxml-child-indent 2)
+ '(package-archives (quote (("melpa" . "https://melpa.org/packages/"))))
+ '(truncate-lines t)
+ '(web-mode-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
