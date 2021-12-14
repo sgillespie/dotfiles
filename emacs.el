22214b18b3f4ffeaaf9e8e8a8bc63317c1abd277
Sean D Gillespie <sean@mistersg.net>
2021-12-14 15:38:32 -0500

    Clean up emacs packages

--- emacs.el
+++ emacs.el
@@ -10,27 +10,22 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
  '(auto-save-mode nil)
- '(backup-directory-alist (quote (("." . "~/.emacs_backups"))))
+ '(backup-directory-alist '(("." . "~/.emacs_backups")))
  '(company-idle-delay nil)
- '(compilation-environment (quote ("TERM=xterm-256color")))
- '(compilation-scroll-output (quote first-error))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(compilation-environment '("TERM=xterm-256color"))
+ '(compilation-scroll-output 'first-error)
+ '(custom-enabled-themes '(sanityinc-tomorrow-night))
  '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(ensime-startup-notification nil)
+   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(fill-column 90)
- '(flycheck-add-next-checker (quote haskell) t)
  '(flycheck-haskell-runghc-command
-   (quote
-    ("/run/current-system/sw/bin/runghc" "--" "-i" "-packageCabal" "-packagebase" "-packagebytestring" "-packagecontainers" "-packageprocess" "-packagedirectory" "-packagefilepath")))
+   '("/run/current-system/sw/bin/runghc" "--" "-i" "-packageCabal" "-packagebase" "-packagebytestring" "-packagecontainers" "-packageprocess" "-packagedirectory" "-packagefilepath"))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
- '(haskell-stylish-on-save t t)
  '(indent-tabs-mode nil)
- '(ivy-use-virtual-buffers t)
  '(ivy-count-format "(%d/%d) ")
+ '(ivy-use-virtual-buffers t)
  '(js-indent-level 2)
  '(js2-allow-member-expr-as-function-name t)
  '(js2-basic-offset 2)
@@ -46,10 +41,9 @@
  '(make-backup-files nil)
  '(neo-window-width 40)
  '(nxml-child-indent 2)
- '(package-archives (quote (("melpa" . "https://melpa.org/packages/"))))
- '(package-selected-packages
-   (quote
-    (helm-company helm format-all flycheck-haskell yaml-mode web-mode use-package thrift terraform-mode smex rjsx-mode restclient nix-mode neotree magit kotlin-mode json-mode intero idris-mode ido-completing-read+ groovy-mode ghc gh-md flycheck-kotlin find-file-in-repository exec-path-from-shell ensime emacsql-mysql dockerfile-mode color-theme-sanityinc-tomorrow color-theme)))
+ '(package-archives
+   '(("melpa" . "https://melpa.org/packages/")
+     ("elpa" . "https://elpa.gnu.org/packages/")))
  '(truncate-lines t)
  '(web-mode-indent-offset 2))
 
@@ -129,9 +123,6 @@
   (package-install 'use-package))
 
 ;; Themes
-(use-package color-theme
-  :ensure t)
-
 (use-package color-theme-sanityinc-tomorrow
   :ensure t
   :config (color-theme-sanityinc-tomorrow-night))
@@ -154,9 +145,6 @@
 (use-package emacsql-mysql
   :ensure t)
 
-(use-package ensime
-  :ensure t)
-
 (use-package flycheck
   :ensure t
   :hook (after-init . global-flycheck-mode)
@@ -166,15 +154,20 @@
           (setq-default flycheck-disabled-checkers
                         (append flycheck-disabled-checkers '(javascript-jshint))))
 
-(use-package flycheck-haskell
+; Haskell
+(use-package lsp-mode
   :ensure t
-  :after (haskell-mode)
-  :hook ((haskell-mode . flycheck-haskell-setup)))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((haskell-mode . lsp))
+  :commands lsp)
+
+(use-package lsp-treemacs :ensure t)
+(use-package lsp-ivy :ensure t)
+(use-package lsp-ui :ensure t)
+(use-package lsp-haskell :ensure t)
 
 (use-package format-all
-  :ensure t)
-
-(use-package ghc
   :ensure t)
 
 (use-package groovy-mode
@@ -183,34 +176,6 @@
          ("\\.gradle$" . groovy-mode))
   :interpreter "groovy"
   :hook (groovy-mode . java-like-mode-hook))
-
-(use-package haskell-mode
-  :ensure t
-  :config (setq-default flycheck-disabled-checkers
-            (append flycheck-disabled-checkers '(haskell-stack-ghc)))
-  :bind (:map haskell-mode-map
-          ("C-c `" . haskell-interactive-bring))
-  :custom (haskell-stylish-on-save t)
-          (flycheck-add-next-checker
-             'haskell
-             '(warning . haskell-hlint))
-  :hook ((haskell-mode . haskell-indentation-mode)
-         (haskell-mode . interactive-haskell-mode)))
-
-(use-package ivy
-  :ensure t
-  :config (ivy-mode 1))
-
-(use-package idris-mode
-  :ensure t)
-
-(use-package intero
-  :ensure t
-  :after (fly-check)
-  :config (intero-global-mode nil)
-          (flycheck-add-next-checker
-           'intero
-           '(warning . haskell-hlint)))
 
 (use-package js2-mode
   :ensure t
@@ -228,10 +193,17 @@
 (use-package kotlin-mode
   :ensure t)
 
+(use-package fsharp-mode
+  :defer t
+  :ensure t)
+
 (use-package flycheck-kotlin
   :ensure t
   :after (flycheck)
   :init (flycheck-kotlin-setup))
+
+(use-package glsl-mode
+  :ensure t)
 
 (use-package ruby-mode
   :mode "capfile")
@@ -243,17 +215,6 @@
   :ensure t
   :mode "\\.js$")
 
-(use-package sbt-mode
-  :ensure t)
-
-(use-package scala-mode
-  :ensure t
-  :bind (:map ensime-mode-map
-              ("C-c C-b RET" . sbt-command)))
-
-(use-package thrift
-  :ensure t)
-
 (use-package web-mode
   :ensure t
   :mode "\\.html$")
@@ -262,19 +223,14 @@
   :ensure t)
 
 ;; Others
-(use-package ansi-color
+(use-package counsel
   :ensure t
-  :config
-  (setq compilation-finish-functions
-        'colorize-compilation))
-  
+  :config (ivy-mode 1))
+
 (use-package exec-path-from-shell
   :if window-system
   :ensure t
   :config (exec-path-from-shell-initialize))
-
-(use-package find-file-in-repository
-  :ensure t)
 
 (use-package magit
   :ensure t
@@ -301,27 +257,7 @@
 
 (add-hook 'java-mode-hook 'java-like-mode-hook)
 
-;; ansi-colorize after a test so terminal colors come through.
-(define-derived-mode ansi-compilation-mode compilation-mode "ansi compilation"
-  "Compilation mode that understands ansi colors."
-  (read-only-mode 0)
-  (ansi-color-apply-on-region (point-min) (point-max)))
-  
-(defun colorize-compilation ()
-  "Ansi colorize the compilation buffer."
-  (ansi-compilation-mode))
-
-(defun open-in-idea ()
-  "Open the current file in intellij IDEA 15 (OS X specific)."
-  (interactive)
-  (when (file-exists-p (buffer-file-name))
-      (start-process-shell-command "idea-community" "*idea*"
-                                   (format "idea-community --line %s %s"
-                                           (line-number-at-pos)
-                                           (buffer-file-name)))))
-
 ;;; Custom variables
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
